4b4bd4971c65cfde8d5fbe7b74b4268a6919a7a3
udsondev <udson.dev@gmail.com>
2021-10-11 09:39:11 -0300

    finalizando a configuração do webpack

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,7 @@ const path = require('path')
 module.exports = {
     entry: path.resolve(__dirname, 'src', 'index.jsx'),
     output: {
-        path.resolve(__dirname, 'dist'),
+        path: path.resolve(__dirname, 'dist'),
         filename: 'bundle.js'
     },
     resolve: {
